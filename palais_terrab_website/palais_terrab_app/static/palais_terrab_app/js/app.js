842f60a64512e1089542190f03dcae2bdcc91736
Zakariae Boutaleb <zakariaeboutaleb@gmail.com>
2017-04-27 22:41:02 +0100

    just the smileys, a whole fucking day for smileys, fuck my life

--- palais_terrab_website/palais_terrab_app/static/palais_terrab_app/js/app.js
+++ palais_terrab_website/palais_terrab_app/static/palais_terrab_app/js/app.js
@@ -1,2 +1,45 @@
 var total_height = $('.header').height();
 $('.first_section-no-bg').css("padding-top", total_height);
+
+$("#frame [class*='emo-']").on("click", function(){
+	if($(this).attr('class') === 'emo-mad'){
+		$('.overlay_emo_mad').css('background-color', '#f9de13');
+
+		$('.overlay_emo_sad').css('background-color', 'transparent');
+		$('.overlay_emo_surprised').css('background-color', 'transparent');
+		$('.overlay_emo_happy').css('background-color', 'transparent');
+		$('.overlay_emo_excited').css('background-color', 'transparent');
+	}
+	else if($(this).attr('class') === 'emo-sad'){
+		$('.overlay_emo_sad').css('background-color', '#f9de13');
+
+		$('.overlay_emo_mad').css('background-color', 'transparent');
+		$('.overlay_emo_surprised').css('background-color', 'transparent');
+		$('.overlay_emo_happy').css('background-color', 'transparent');
+		$('.overlay_emo_excited').css('background-color', 'transparent');
+	}
+	else if($(this).attr('class') === 'emo-surprised'){
+		$('.overlay_emo_surprised').css('background-color', '#f9de13');
+
+		$('.overlay_emo_mad').css('background-color', 'transparent');
+		$('.overlay_emo_sad').css('background-color', 'transparent');
+		$('.overlay_emo_happy').css('background-color', 'transparent');
+		$('.overlay_emo_excited').css('background-color', 'transparent');
+	}
+	else if($(this).attr('class') === 'emo-happy'){
+		$('.overlay_emo_happy').css('background-color', '#f9de13');
+
+		$('.overlay_emo_mad').css('background-color', 'transparent');
+		$('.overlay_emo_sad').css('background-color', 'transparent');
+		$('.overlay_emo_surprised').css('background-color', 'transparent');
+		$('.overlay_emo_excited').css('background-color', 'transparent');
+	}
+	else if($(this).attr('class') === 'emo-excited'){
+		$('.overlay_emo_excited').css('background-color', '#f9de13');
+		
+		$('.overlay_emo_mad').css('background-color', 'transparent');
+		$('.overlay_emo_sad').css('background-color', 'transparent');
+		$('.overlay_emo_surprised').css('background-color', 'transparent');
+		$('.overlay_emo_happy').css('background-color', 'transparent');
+	}
+});
